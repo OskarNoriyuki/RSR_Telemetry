3f89fb899b170923b6df4041348c328b15f0fd29
OskarNoriyuki <57155385+OskarNoriyuki@users.noreply.github.com>
2022-11-10 13:14:10 -0300

    update - reading A1332 angle

--- Serial Comm/panel_mega_simulator/src/main.cpp
+++ Serial Comm/panel_mega_simulator/src/main.cpp
@@ -7,8 +7,13 @@
 // A1332 #2 -> 0X0E
 #define Addr 0x0C
 
+#define ANGLE_DATA_FORMAT "{ANG=%04d}"
 static const char *TEST_STRING = "{testando1234}";
 char sendBuf[100] = {0};
+
+//prototypes
+static int fReadAngleRaw(void);
+static float fReadAngle(void);
 
 void setup() {
   Wire.begin();
@@ -17,7 +22,8 @@ void setup() {
 }
 
 void loop() {
-  strcpy(sendBuf, TEST_STRING);
+  int angleRaw = fReadAngleRaw();
+  sprintf(sendBuf, ANGLE_DATA_FORMAT, angleRaw);
   //send string via USART1
   Serial.print("Enviando... \"");
   Serial.print(sendBuf);
@@ -30,19 +36,26 @@ void loop() {
   Serial.flush();
   Serial2.flush();
 
-      unsigned int data[2];
-      Wire.beginTransmission(Addr); // Start I2C Transmission 
-      Wire.endTransmission();       // Stop I2C Transmission 
-      Wire.requestFrom(Addr, 2);    // Request 2 byte of data
-      if(Wire.available() == 2){    // Read 2 bytes of data: raw_adc msb, raw_adc lsb 
-        data[0] = Wire.read();
-        data[1] = Wire.read();
-      }
-      // Convert the data to 12-bits
-      int raw_angle = ((data[0] & 0x0F) * 256) + (data[1] & 0xFF);
-      float angle = (raw_angle * 360.0) / 4096.0;
-      Serial.print("Sensor value: ");
-      Serial.println(angle);
-
   delay(400);
+}
+
+static int fReadAngleRaw(){
+  unsigned int data[2];
+  Wire.beginTransmission(Addr); // Start I2C Transmission 
+  Wire.endTransmission();       // Stop I2C Transmission 
+  Wire.requestFrom(Addr, 2);    // Request 2 byte of data
+  delayMicroseconds(100);
+  if(Wire.available() == 2){    // Read 2 bytes of data: raw_adc msb, raw_adc lsb 
+    data[0] = Wire.read();
+    data[1] = Wire.read();
+  }
+  // Convert the data to 12-bits
+  int raw_angle = ((data[0] & 0x0F) * 256) + (data[1] & 0xFF);
+  return raw_angle;
+}
+
+static float fReadAngle(){
+  int raw_angle = fReadAngleRaw();
+  float angle = (raw_angle * 360.0) / 4096.0;
+  return angle;
 }
